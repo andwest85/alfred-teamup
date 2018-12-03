dc41859ebb66232b81b685f4b689397c1e34c11f
andwest85 <andwest@cisco.com>
2018-12-03 14:35:24 -0800

    modified controller.hears function

--- skills/keywords.js
+++ skills/keywords.js
@@ -1,7 +1,7 @@
 module.exports =  function (controller) {
 
 
-  controller.hears(['domain'], 'direct_message,direct_mention', function (bot, message) {
+  controller.hears(["domain"], 'direct_message,direct_mention', function (bot, message) {
 
       var text = "You can add and verify your domains in the " + "Webex Control Hub".link('https://admin.webex.com/login') "to ensure the security and integrity of your organization and help with user management.";
       bot.reply(message, text);
