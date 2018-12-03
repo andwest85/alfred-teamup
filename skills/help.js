5d27f5356b7191cd0064d312b49001ff54f376f6
andwest85 <andwest@cisco.com>
2018-12-03 14:51:44 -0800

    more vocabulary

--- skills/help.js
+++ skills/help.js
@@ -3,14 +3,6 @@
 //
 module.exports = function (controller) {
 
-
-    controller.hears(["domain"], 'direct_message,direct_mention', function (bot, message) {
-
-        var text = "You can add and verify your domains in the " + "Webex Control Hub".link('https://admin.webex.com/login') + "to ensure the security and integrity of your organization and help with user management.";
-        bot.reply(message, text);
-
-    });
-
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
         text += "\n- " + bot.appendMention(message, "Keyword") + ": I will respond to keywords with information and resources to help you adopt Webex Teams. (i.e. notification, create space; here is a complete list: https://cisco.box.com/shared/static/7puqq5bsleeiaclgdyuyjg93fmlflzdw.docx)";
