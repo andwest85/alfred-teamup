e825eaae0f8d1c322d737a856e4f986d0af0cfe9
andwest85 <andwest@cisco.com>
2018-12-03 16:14:26 -0800

    checking syntax

--- skills/keywords.js
+++ skills/keywords.js
@@ -1,10 +1,10 @@
 module.exports =  function (controller, bot) {
 
-
-  controller.hears(["domain"], 'direct_message,direct_mention', function (bot, message) {
-      var text = "You can add and verify your domains in the " + "Webex Control Hub".link('https://admin.webex.com/login') " to ensure the security and integrity of your organization and help with user management.";
-      bot.reply(message, text);
-  });
+  //
+  // controller.hears(["domain"], 'direct_message,direct_mention', function (bot, message) {
+  //     var text = "You can add and verify your domains in the " + "Webex Control Hub".link('https://admin.webex.com/login') " to ensure the security and integrity of your organization and help with user management.";
+  //     bot.reply(message, text);
+  // });
 
   controller.hears(["announcement"], 'direct_message,direct_mention', function (bot, message) {
       var text = "Some company departments like Human Resources and Corporate Communications need to ensure that everyone is informed about the latest news. If you only use traditional methods like email, many people don’t get around to reading updates and there’s very little feedback. How about taking the formality down a notch? Create an announcement space in Webex Teams, so you can post bite-size updates and links to the latest information.";
--- skills/z-fallback.js
+++ skills/z-fallback.js
@@ -7,4 +7,5 @@ module.exports = function (controller) {
         var mardown = "I apologize; I am not familiar with that phrase. Please refer to this list of search terms - https://cisco.box.com/shared/static/7puqq5bsleeiaclgdyuyjg93fmlflzdw.docx. If your topic is not present, type 'Cisco' and we can search the at your service portal and other Cisco databases. Finally, type 'escalate' and we will find some in-person support."
         bot.reply(message, mardown);
     });
+
 }
