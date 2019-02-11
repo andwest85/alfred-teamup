9c5b8a1d4e64c262fc58a906d9e3c2dfc7e7688b
andwest85 <andwest@cisco.com>
2019-02-11 15:46:25 -0800

    changed welcome message

--- skills/welcome.js
+++ skills/welcome.js
@@ -5,8 +5,7 @@ module.exports = function (controller) {
 
     controller.on('bot_space_join', function (bot, event) {
 
-        var welcome = "Hi <@personId:${event.actorId}>, delighted to make your acquaintence! As part of the adoption campaign for Webex Teams, I am here to support you. I provide information on key topics like "
-        + "notifications and creating spaces. If I do not have pertinent Webex-related information, I will search the At Your Service portal.";
+        var welcome = `Hi <@personId:${event.actorId}>, so glad meeting you!`;
 
         if (this.identity) {
             welcome += `<br/>I am the **${this.identity.displayName}** bot`;
@@ -21,8 +20,7 @@ module.exports = function (controller) {
                 return;
             }
 
-            var help = "As part of the adoption campaign for Webex Teams, I am here to support you. I provide information on key topics like "
-            + "notifications and creating spaces. If I do not have pertinent Webex-related information on the topic you entered, I will search the At Your Service portal.";
+            var help = "Type `help` to learn about my skills.";
 
             if (rawMessage.roomType == "group") {
                 help = "Note that this is a 'Group' space. I will answer only if mentionned.<br/>";
@@ -41,3 +39,49 @@ module.exports = function (controller) {
         });
     });
 }
+
+// //
+// // Welcome message sent as the bot is added to a space
+// //
+// module.exports = function (controller) {
+//
+// //<@personId:${event.actorId}>
+//
+//     controller.on('bot_space_join', function (bot, event) {
+//
+//         var welcome = "Hi, delighted to make your acquaintence! As part of the adoption campaign for Webex Teams, I am here to support you. I provide information on key topics like "
+//         + "notifications and creating spaces. If I do not have pertinent Webex-related information, I will search the At Your Service portal.";
+//
+//         if (this.identity) {
+//             welcome += `<br/>I am the **${this.identity.displayName}** bot`;
+//         }
+//
+//         bot.say ({
+//             text: welcome,
+//             channel: event.channel
+//         }, function (err, rawMessage) {
+//             if (err) {
+//                 console.log("Error while posting back welcome message, err: " + err.message)
+//                 return;
+//             }
+//
+//             var help = "As part of the adoption campaign for Webex Teams, I am here to support you. I provide information on key topics like "
+//             + "notifications and creating spaces. If I do not have pertinent Webex-related information on the topic you entered, I will search the At Your Service portal.";
+//
+//             if (rawMessage.roomType == "group") {
+//                 help = "Note that this is a 'Group' space. I will answer only if mentionned.<br/>";
+//                 help += "To learn about my skills, type " + bot.appendMention(rawMessage, "help");
+//             }
+//
+//             bot.say({
+//                 text: `_${help}_`,
+//                 channel: rawMessage.roomId
+//             }, function (err, messageAck) {
+//                 if (err) {
+//                     console.log("Error while postig back help message, err: " + err.message)
+//                     return;
+//                 }
+//             });
+//         });
+//     });
+// }
